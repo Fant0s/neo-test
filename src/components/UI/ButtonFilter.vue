63cfcd4eed25c88dbeecde1a54f816abd589d14b
Alex <110283025+Fant0s@users.noreply.github.com>
2025-06-17 17:09:32 +0500

    add button for filters

---
+++ src/components/UI/ButtonFilter.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+const props = defineProps<{ active: boolean; text: string }>()
+
+defineEmits<{
+  (e: 'click'): void
+}>()
+</script>
+
+<template>
+  <button class="btn" :class="{ active: props.active }" @click="$emit('click')">
+    {{ props.text }}
+  </button>
+</template>
+
+<style lang="sass" scoped>
+.btn
+  background-color: white
+  &.active
+    background-color: green
+</style>
