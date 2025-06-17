cb7349c5c2994e89f9306753e5f4a426bbbaa86b
Alex <110283025+Fant0s@users.noreply.github.com>
2025-06-17 17:11:52 +0500

    fix some styles

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,31 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import TaskForm from '@/components/TaskForm.vue'
+import TaskFilter from '@/components/TaskFilter.vue'
+import TaskList from '@/components/TaskList.vue'
+</script>
 
 <template>
-  <header>test</header>
-
-  <main></main>
+  <header>Задачи</header>
+  <main>
+    <section class="add-section">
+      <TaskForm />
+    </section>
+    <section class="tasks-section">
+      <div class="filters">
+        <TaskFilter />
+      </div>
+      <TaskList />
+    </section>
+  </main>
 </template>
 
-<style scoped></style>
+<style lang="sass" scoped>
+
+main
+  display: flex
+  flex-direction: column
+  flex: 1
+  max-width: 800px
+  margin: 0 auto
+  width: 100%
+</style>
